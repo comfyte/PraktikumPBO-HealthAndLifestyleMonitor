0bffb27e14769ca12cb50b1cc45f107b3033b88b
Farrel R <rafirizqy12@gmail.com>
2020-05-20 22:22:32 +0700

    Test new database migration for JadwalObat

--- HealthAndLifestyleMonitor/DatabaseModels/JadwalObatModel.cs
+++ HealthAndLifestyleMonitor/DatabaseModels/JadwalObatModel.cs
@@ -10,8 +10,6 @@ namespace HealthAndLifestyleMonitor.DatabaseModels
         public string Nama { get; set; }
         public string Deskripsi { get; set; }
         public string Waktu { get; set; }
-        //public bool Mingguan { get; set; }
-        // Use blank Hari for daily
         public string Hari { get; set; }
     }
 }
