2037777c1d1f0d37ceb25d00370fe7c619efd580
Farrel R <rafirizqy12@gmail.com>
2020-05-22 21:16:05 +0700

    Initial Cuaca API logic

--- HealthAndLifestyleMonitor/Cuaca.cs
+++ HealthAndLifestyleMonitor/Cuaca.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Windows;
 
 namespace HealthAndLifestyleMonitor
 {
     public class Cuaca
     {
+        private string _apiKey = "d383e9ec57f3ab586ea47d2124225d90";
+
+        public void GetCuaca()
+        {
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/weather?q=yogyakarta&lang=id&appid=d383e9ec57f3ab586ea47d2124225d90");
+            var request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            CuacaObject testobje = JsonSerializer.Deserialize<CuacaObject>(response.Content);
+
+            MessageBox.Show(testobje.ToString());
+        }
+        public bool CekLokasi()
+        {
+            return true;
+        }
     }
 }
---
+++ HealthAndLifestyleMonitor/CuacaObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthAndLifestyleMonitor
+{
+    public class CuacaObject
+    {
+        public int cod { get; set; }
+        public string name { get; set; }
+        public List<DetailCuacaObject> weather { get; set; }
+        public TemperaturCuacaObject main { get; set; }
+        public KoordinatObject coord { get; set; }
+
+    }
+
+    public class DetailCuacaObject
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+    }
+
+    public class TemperaturCuacaObject
+    {
+        public double temp { get; set; }
+    }
+
+    public class KoordinatObject
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+}
